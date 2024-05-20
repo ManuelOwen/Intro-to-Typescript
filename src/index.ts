ae443cb22c84d5c115300ce7dbc1f23aef359c02
ManuelOwen <manuelowen5324f4e@gmail.com>
2024-05-20 14:16:13 +0300

    intro to typescript

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// function add(a: number, b: number) : number{
+//   return a + b;
+// }
+// console.log(add(1790, 2)); // 3
+import variables from './variables'
+
+variables();
---
+++ src/variables.ts
@@ -0,0 +1,18 @@
+const variables = () => {
+      let a: number = 10;
+      let b: string = "hello word";
+      let boolean: boolean = true;
+      let evenNumbers: number[]= [2, 4, 4, 8, 10, 12, 14, 16, 18, 20];
+      let g: any = 10;
+
+
+
+      console.log(`Type of a: ${typeof a} value : ${a}`)
+      console.log(`type of b: ${typeof b} value :${b}`)
+      console.log(`type of boolean: ${typeof boolean} value :${boolean}`)
+      console.log(`type of evenNumbers: ${typeof evenNumbers} && value :${evenNumbers}`)
+      console.log(`type of g:${typeof g}&& value :${g}`)
+
+
+}
+export default variables;
